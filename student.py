6402a055745206086f3ce39a6a8356ac189af94a
Vib <vibofl1@gmail.com>
2023-04-07 01:40:24 +0530

    self, insance, static methods  usage in a class

---
+++ student.py
@@ -0,0 +1,40 @@
+class Student:
+    passingpercentage=40
+    #instance method 
+    def studentdetails(self):
+        self.name="vib"
+        self.age=22
+        #percentage=88 can be accessed locally only 
+        self.percentage=90 #now it can accessed as it is a instance attribute 
+        print(self.name)
+        print(self.percentage)
+
+        
+        # print(percentage)
+    
+    def ispassed(self):
+        if Student.passingpercentage<self.percentage:
+            print("yes")
+        else:
+            print("NO")
+    # for static method  use Decroator  # if required without self and to  avoid this error  welcometoschool() takes 0 positional arguments but 1 was given
+    
+    @staticmethod
+    def welcometoschool():
+        print( "Hey! welcome to school ")
+        
+        
+    
+
+
+s1=Student()
+print(s1)
+# print(s1.studentdetails(). 
+s1.studentdetails()
+s1.ispassed()
+s1.welcometoschool()
+
+
+
+
+        
